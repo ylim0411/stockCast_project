e86dfdd93cc8932266b8621ae678a5a6f87eb809
youlim <yunsupin@gmail.com>
2025-08-04 10:45:37 +0900

    250804_Lim - productCategory : 목록 조회, 접기 구현(펼치기 추가 구현 예정) → 카테고리 등록, 수정 팝업 구현 예정 - product : 목록 조회, 삭제 구현(등록, 수정 구현 예정)

--- src/main/java/com/spring/stockCast/controller/ProductController.java
+++ src/main/java/com/spring/stockCast/controller/ProductController.java
@@ -1,8 +1,11 @@
 package com.spring.stockCast.controller;
 
+import com.spring.stockCast.dto.ProductCategoryDTO;
+import com.spring.stockCast.service.ProductCategoryService;
 import com.spring.stockCast.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,11 +19,27 @@ import java.util.Map;
 @RequestMapping("/product")
 public class ProductController {
     private final ProductService productService;
+    private final ProductCategoryService productCategoryService;
 
     @GetMapping("/byClient")
     @ResponseBody
     public List<Map<String, Object>> getProductsByClient(@RequestParam int clientId) {
         return productService.getProductsByClientId(clientId);
+    }
+
+    @GetMapping("/")
+    public String productList(Model model) {
+        List<ProductCategoryDTO> categoryList = productCategoryService.categorySelect();
+        model.addAttribute("categoryList", categoryList);
+
+        return "product";
+    }
+
+    @GetMapping("/delete")
+    public String productDelete(@RequestParam("id") int productId) {
+        System.out.println("delete?" + productId);
+        productService.delete(productId);
+        return "redirect:/product/";
     }
 
 }
